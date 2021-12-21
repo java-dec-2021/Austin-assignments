929d871233978dd8e93c7b41564952f1a485e34b
Austin Nye <amnye0815@gmail.com>
2021-12-21 11:50:21 -0800

    finished phone.java

---
+++ 02-java-oop/09-phone/Phone.java
@@ -0,0 +1,54 @@
+public abstract class Phone {
+
+    // attributes
+    private String versionNumber;
+    private int batteryPercentage;
+    private String carrier;
+    private String ringTone;
+
+    public Phone(String versionNumber, int batteryPercentage, String carrier, String ringTone){
+        this.versionNumber = versionNumber;
+        this.batteryPercentage = batteryPercentage;
+        this.carrier = carrier;
+        this.ringTone = ringTone;
+    }
+
+    // abstract method. This method will be implemented by the subclasses
+    public abstract void displayInfo();
+
+    // setters
+    public void setVersionNumber(String vN){
+        this.versionNumber = vN;
+    }
+
+    public void setBatteryPercentage(int bP){
+        this.batteryPercentage = bP;
+    }
+
+    public void setCarrier(String c){
+        this.carrier = c;
+    }
+
+    public void setRingTone(String rT){
+        this.ringTone = rT;
+    }
+
+    // getters
+    public String getVersionNumber(){
+        return this.versionNumber;
+    }
+
+    public int getBatteryPercentage(){
+        return this.batteryPercentage;
+    }
+
+    public String getCarrier(){
+        return this.carrier;
+    }
+
+    public String getRingTone(){
+        return this.ringTone;
+    }
+
+}
+
